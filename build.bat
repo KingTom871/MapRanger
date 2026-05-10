d8f9f7c8ffe2af59df7362fc595c70f6b59db50d
EhsanMalik <em1130mail@gmail.com>
2026-05-10 17:44:14 +0800

    build: update build.bat

--- build.bat
+++ build.bat
@@ -8,18 +8,15 @@ if not exist "build\obj" (
     mkdir build\obj
 )
 
-cl ^
-/std:c++20 ^
-/EHsc ^
-/I. ^
-/Iincludes ^
-main.cpp ^
-runtime/Runtime.cpp ^
-utils/Math.cpp ^
+set sourceFiles=main.cpp ^
+runtime\runtime.cpp ^
+utils\Math.cpp ^
+utils\Capture.cpp
+
+cl /std:c++20 /EHsc /I. /Iincludes %sourceFiles% ^
 /Fobuild\obj\ ^
 /Febuild\MapRanger.exe ^
-user32.lib ^
-gdi32.lib
+user32.lib gdi32.lib
 
 if %ERRORLEVEL% equ 0 (
     echo Build successful.
