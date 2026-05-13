0eb363223054e5d149131b1986689a6e81c94c9b
EhsanMalik <em1130mail@gmail.com>
2026-05-13 23:17:21 +0800

    build: add Dear ImGui in build.bat

--- build.bat
+++ build.bat
@@ -12,12 +12,19 @@ set sourceFiles=main.cpp ^
 runtime\runtime.cpp ^
 utils\Math.cpp ^
 utils\Capture.cpp ^
-utils\Input.cpp
+utils\Input.cpp ^
+graphics\GraphicsContext.cpp ^
+includes\ImGui\imgui.cpp ^
+includes\ImGui\imgui_draw.cpp ^
+includes\ImGui\imgui_tables.cpp ^
+includes\ImGui\imgui_widgets.cpp ^
+includes\ImGui\backends\imgui_impl_win32.cpp ^
+includes\ImGui\backends\imgui_impl_dx11.cpp
 
-cl /std:c++20 /EHsc /I. /Iincludes %sourceFiles% ^
+cl /std:c++20 /EHsc /I. /Iincludes /I includes/ImGui %sourceFiles% ^
 /Fobuild\obj\ ^
 /Febuild\MapRanger.exe ^
-user32.lib gdi32.lib
+user32.lib gdi32.lib d3d11.lib dxgi.lib imm32.lib
 
 if %ERRORLEVEL% equ 0 (
     echo Build successful.
